95b0dd404180727f050a5d05d5bdebea29af8d5e
Pascal Fleury <fleury@google.com>
2026-04-15 13:36:16 +0000

    M home/.config/i3/rename-workspace.sh  M home/.config/polybar/config.ini

--- home/.config/i3/rename-workspace.sh
+++ home/.config/i3/rename-workspace.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-new_name=$(i3-input -P "New workspace name: " | sed 's/^"//;s/"$//')
-[[ -n "$newName" ]] && i3-msg rename workspace to "$new_name"
+[[ -z "$(which jq)" ]] && echo "No 'jq' command!"
+[[ -z "$(which kdialog)" ]] && echo "No 'kdialog' command!"
+
+num=$(i3-msg -t get_workspaces | jq '.[] | select(.focused==true).num'| cut -d"\"" -f2)
+
+new_name=$(kdialog --inputbox "Workspace #${num} new name: ")
+[[ -n "${new_name}" ]] && i3-msg rename workspace to "${num}:${new_name}"
